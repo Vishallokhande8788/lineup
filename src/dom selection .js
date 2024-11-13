34bb1f87c2f691abe8d7c7d2ef596cbb515386f7
vishal lokhande <lvishal108@gmail.com>
2024-11-13 14:40:48 +0000

    implement random logic

---
+++ src/dom selection .js
@@ -0,0 +1,4 @@
+const inputEL = document.querySelector("input");
+const formEL = document.querySelector("form");
+const yearEL = document.querySelector(".year");
+export { inputEL, formEL, yearEL };
--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
 import "./index.css";
+import { inputEL, formEL, yearEL } from "./dom selection .js";
+// Mark : dom selection
+//IIFE
+
+(function () {
+  const year = new Date().getFullYear();
+
+  yearEL.textContent = `${year}`;
+})();
+
+const tasks = [];
+formEL.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!inputEL.value) {
+    return;
+  }
+
+  tasks.push({
+    title: inputEL.value,
+    isCompleted: false,
+    id: crypto.randomUUID(),
+  });
+
+  console.log(tasks);
+  inputEL.value = "";
+});
