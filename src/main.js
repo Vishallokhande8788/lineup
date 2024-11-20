1167bac8d02ac7a913b6a30ab7820f0fe9666fed
vishal lokhande <lvishal108@gmail.com>
2024-11-20 13:50:56 +0000

    persiststate on refresh

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import "./index.css";
 import { inputEL, formEL, yearEL, taskListEL } from "./dom selection .js";
 import Task from "./components/Task.js";
-import localforage from "localforage";
+import localforage, { setItem } from "localforage";
 // Mark : dom selection
 //IIFE
 
@@ -11,34 +11,42 @@ import localforage from "localforage";
   yearEL.textContent = `${year}`;
 })();
 
-let tasks = [];
+let state = [];
+
+localforage.getItem("tasks").then((tasks) => {
+  if (tasks) {
+    state = tasks;
+    console.log(state);
+    renderTasks();
+  }
+});
 
 // localStorage.setDriver(localforage.LOCALSTORAGE);
 
 //IIFE
-(async ()=>{
-  const data=await localforage.getItem('device');
+async () => {
+  const data = await localforage.getItem("device");
   console.log(data);
-})
-
+};
 
 // this toggles the isCompleted property of a task
 //will be called when the user clicks on the checkbox
 function toggleTask(id) {
-  tasks = tasks.map((task) => {
+  state = state.map((task) => {
     if (task.id === id) {
       return { ...task, isCompleted: !task.isCompleted };
     }
     return task;
   });
 }
-//show uncompleted tasks first
-tasks.sort((a, b) => a.isCompleted - b.isCompleted);
+//show uncompleted state first
+state.sort((a, b) => a.isCompleted - b.isCompleted);
+localforage.setItem("tasks", state);
 
 function renderTasks() {
   taskListEL.innerHTML = "";
   const fragment = document.createDocumentFragment();
-  tasks.forEach((task) => {
+  state.forEach((task) => {
     const taskEl = Task(task.value, task.isCompleted, task.id);
 
     fragment.appendChild(taskEl);
@@ -53,24 +61,26 @@ formEL.addEventListener("submit", (e) => {
     return;
   }
 
-  tasks.unshift({
+  state.unshift({
     id: crypto.randomUUID(),
     value: inputEL.value,
     isCompleted: false,
   });
-  console.log(tasks);
+  console.log(state);
+  localforage.setItem("tasks", state);
+
   inputEL.value = "";
 
   taskListEL.addEventListener("click", (e) => {
     if (e.target.targetName === "INPUT") {
       console.log(e.target.closest("label").id);
-      toggleTask();
+      toggleTask(e.target.closest("label").id);
       renderTasks();
     }
   });
 
   toggleTask;
-  // console.log(tasks);
+  // console.log(state);
   renderTasks();
   inputEL.value = "";
 });
